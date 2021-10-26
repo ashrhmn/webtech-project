86af442f49fd437e7e20baef70b01a354e95b4c8
Ashik Rahman <ashrhmn99@gmail.com>
2021-10-27 03:28:41 +0600

    Move login func to userrepo

--- controllers/login.php
+++ controllers/login.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 
 if (isset($_POST['login'])) {
 
@@ -10,52 +9,27 @@ if (isset($_POST['login'])) {
         echo 'Invalid username/email and/or password';
         return;
     }
-    require_once('../repository/database.php');
 
-    $sql = "select * from  users where username='" . $usernameOrEmail . "' or email='" . $usernameOrEmail . "'";
-    $result = query($sql);
-
-    if ($result != false && $result->num_rows > 0) {
-        if ($row = $result->fetch_assoc()) {
-            if ($row['password'] == $password) {
-                //Login Succcessful
-                // echo 'Logged in succ';
-
-                $token = base64_encode(random_bytes(37));
-                setcookie('token', $token, time() + (365 * 24 * 3600), '/');
-                if (mutate("insert into session (username,token) values('" . $row['username'] . "','" . $token . "')")) {
-                    header('location: ../views/dashboard.php');
-                } else {
-                    echo 'Error creating session, internal error';
-                    return;
-                }
-                return;
-            } else {
-                echo 'Wrong Password';
-                return;
-            }
-        }
+    require_once('../repository/UserRepo.php');
+    $status = credsStatus($usernameOrEmail, $password); //-> 1=loginSucc, 0=wrongPassword, -1=userNotFound, 2=sesionError
+    switch($status){
+        case 1:
+            header('location: ../views/dashboard.php');
+            break;
+        case 0:
+            echo 'Wrong Password';
+            break;
+        case -1:
+            echo 'User not found with the username or email';
+            break;
+        case 2:
+            echo 'Error creating session, internal error';
+            break;
+        default:
+            echo 'Unknown error occured';
+            break;
     }
-    echo 'User not found with the username or email';
     return;
 }
-
 echo 'Error form submission';
 return;
-
-echo 'Login Handler';
-setcookie('isLoggedIn', 'true', time() + 3600, '/');
-
-
-
-$conn = getConnection();
-
-$res = $conn->query('select * from users');
-
-if ($res->num_rows > 0) {
-    while ($row = $res->fetch_assoc()) {
-        echo $row['name'];
-    }
-} else {
-    echo '0 res';
-}
