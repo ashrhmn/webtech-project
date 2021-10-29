68144e87f14d503b011be072d8a83466fb88f12a
Ashik Rahman <ashrhmn99@gmail.com>
2021-10-30 02:54:16 +0600

    Complete manage session

--- repository/SettingsRepo.php
+++ repository/SettingsRepo.php
@@ -19,7 +19,7 @@ function changePassword($oldPassword, $newpassword) //notAuthenticated=-1, wrong
     while (!feof($usersFile)) {
         $data = fgets($usersFile);
         if ($data != "") {
-            $user = explode('|',$data);
+            $user = explode('|', $data);
             if (trim($user[9]) == $oldPassword) {
                 $modedUser = trim($user[0]) . "|" . trim($user[1]) . "|" . trim($user[2]) . "|" . trim($user[3]) . "|" . trim($user[4]) . "|" . trim($user[5]) . "|" . trim($user[6]) . "|" . trim($user[7]) . "|" . trim($user[8]) . "|" . $newpassword;
                 array_push($dummyUsers, $modedUser);
@@ -52,17 +52,52 @@ function getAllSession()
     }
     $sessions = array();
 
-    $sessionFile = fopen($session_txt,'r');
+    $sessionFile = fopen($session_txt, 'r');
 
-    while(!feof($sessionFile)){
+    while (!feof($sessionFile)) {
         $data = fgets($sessionFile);
-        if($data!=""){
-            $sessionData = explode('|',$data);
-            if(trim($sessionData[0])==$username){
-                array_push($sessions,trim($sessionData[1]));
+        if ($data != "") {
+            $sessionData = explode('|', $data);
+            if (trim($sessionData[0]) == $username) {
+                array_push($sessions, trim($sessionData[1]));
             }
         }
     }
     return $sessions;
+}
 
+function deleteSession($token) // notAuthenticated = -1, notAuthorized = 0, deleteSuccess = 1
+{
+    global $session_txt;
+    $username = getLoggedInUsername();
+    if (!$username) {
+        return -1;
+    }
+    $sessions = array();
+    $sessionFile = fopen($session_txt, 'r');
+    $flag = false;
+    while (!feof($sessionFile)) {
+        $data = fgets($sessionFile);
+        if ($data != "") {
+            $sessionData = explode('|', $data);
+            if (trim($sessionData[0]) == $username && trim($sessionData[1]) == $token) {
+                $flag = true;
+            } else {
+                array_push($sessions, trim($data));
+            }
+        }
+    }
+    fclose($sessionFile);
+
+    if (!$flag) {
+        return 0;
+    }
+    $sessionFile = fopen($session_txt, 'w');
+
+    for ($i = 0; $i < count($sessions); ++$i) {
+        fwrite($sessionFile, $sessions[$i] . "\n");
+    }
+
+    fclose($sessionFile);
+    return 1;
 }
--- views/dashboard/accountSettings/manageSession.php
+++ views/dashboard/accountSettings/manageSession.php
@@ -2,10 +2,25 @@
 
 echo 'Manage Session';
 
+require_once('../../repository/SettingsRepo.php');
+
+$sessions = getAllSession();
+
+if (!$sessions) {
+    echo 'Not authenticated';
+    return;
+}
 ?>
 
 <table>
-    <tr>
-        
-    </tr>
+    <?php
+    for ($i = 0; $i < count($sessions); ++$i) {
+    ?>
+        <tr>
+            <td><?= $sessions[$i] ?><?= $sessions[$i]==$_COOKIE['token']?' (current) ':''  ?></td>
+            <td><a href="../../../controllers/AccountSettings/deleteSession.php?token=<?= $sessions[$i] ?>">Revoke</a></td>
+        </tr>
+    <?php
+    }
+    ?>
 </table>
