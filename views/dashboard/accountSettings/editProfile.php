dcdf96f0ad6c297aa24828cbddf8b021880f26b8
Ashik Rahman <ashrhmn99@gmail.com>
2021-10-30 11:43:56 +0600

    _sync

--- views/dashboard/accountSettings/editProfile.php
+++ views/dashboard/accountSettings/editProfile.php
@@ -1,3 +1,22 @@
 <?php
 
 echo 'Edit Profile';
+
+require_once('../../repository/AuthRepo.php');
+
+$user = getLoggedInUser();
+
+if(!$user){
+    echo 'Not authenticated';
+    return;
+}
+
+?>
+
+<form action="../../controllers/saveProfileEdits.php">
+    <table>
+        <tr>
+            <td></td>
+        </tr>
+    </table>
+</form>
