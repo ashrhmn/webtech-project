1025aa0e87d8307cab04bfe559edfd206e4c9dba
Ashik Rahman <ashrhmn99@gmail.com>
2021-10-31 15:01:39 +0600

    Arrange dashboard

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -4,39 +4,30 @@ require_once('header.php');
 
 print_r($user);
 ?>
-
 <h1>Welcome <?= $user['name'] ?></h1>
-
-<?php
-include('./roles/' . strtolower($user['role']) . '/dashboard.php');
-?>
-
-
 <a href="../controllers/logout.php">Logout</a>
-
 <br>
-
 <form action="#" method="POST">
     <input type="submit" name="editProfile" value="Edit Profile">
     <input type="submit" name="changePassword" value="Change Password">
     <input type="submit" name="manageSession" value="Manage Session">
+    <div>
+        <?php
+            if (isset($_POST['editProfile'])) {
+                ?><input type="submit" name="close" value="Close"><?php
+                include('accountSettings/editProfile.php');
+            }
+            if (isset($_POST['changePassword'])) {
+                ?><input type="submit" name="close" value="Close"><?php
+                include('accountSettings/changePassword.php');
+            }
+            if (isset($_POST['manageSession'])) {
+                ?><input type="submit" name="close" value="Close"><?php
+                include('accountSettings/manageSession.php');
+            }
+        ?>
+    </div>
 </form>
-
-<div><?php
-        if (isset($_POST['editProfile'])) {
-            include('accountSettings/editProfile.php');
-            return;
-        }
-        if (isset($_POST['changePassword'])) {
-            include('accountSettings/changePassword.php');
-            return;
-        }
-        if (isset($_POST['manageSession'])) {
-            include('accountSettings/manageSession.php');
-            return;
-        }
-        echo 'nothing selected';
-        ?></div>
-
 <?php
+include('./roles/' . strtolower($user['role']) . '/dashboard.php');
 return;
