dd104580ffb05c8585a79bb49bbc00dc4926f2ef
Ashik Rahman <ashrhmn99@gmail.com>
2021-11-01 01:44:22 +0600

    Complete editUsers of admin

--- repository/UserRepo.php
+++ repository/UserRepo.php
@@ -84,6 +84,52 @@ function saveUserProfileEdits($user)
     return 1;
 }
 
+function saveUserEdits($user,$id)
+{ //usernameUnavailable=-1, //emailAlreadyExists=0, //userNotFound=-2 saveSuccessfully=1
+
+    require_once('AuthRepo.php');
+    $authUser = getUserById($id);
+    if ($authUser['username'] != $user['username']) {
+        if (!isUsernameAvailable($user['username'])) {
+            return -1;
+        }
+    }
+    if ($authUser['email'] != $user['email']) {
+        if (!isEmailAvailable($user['email'])) {
+            return 0;
+        }
+    }
+
+    global $users_txt;
+    $dummyUsers = array();
+    $usersFile = fopen($users_txt, 'r');
+    $isUserFound = false;
+    while (!feof($usersFile)) {
+        $data = fgets($usersFile);
+        if ($data != "") {
+            $oldUser = explode('|', $data);
+            if (trim($oldUser[0]) == $authUser['id']) {
+                $modedUser = trim($oldUser[0]) . "|" . $user['username'] . "|" . $user['email'] . "|" . $user['name'] . "|" . $user['role'] . "|" . $user['address'] . "|" . $user['gender'] . "|" . $user['dateOfBirth'] . "|" . $user['phone'] . "|" . trim($oldUser[9]);
+                array_push($dummyUsers, $modedUser);
+                $isUserFound = true;
+            } else {
+                array_push($dummyUsers, trim($data));
+            }
+        }
+    }
+    fclose($usersFile);
+    if (!$isUserFound) {
+        return -2;
+    }
+
+    $usersFile = fopen($users_txt, 'w');
+    for ($i = 0; $i < count($dummyUsers); ++$i) {
+        fwrite($usersFile, $dummyUsers[$i] . "\n");
+    }
+    fclose($usersFile);
+    return 1;
+}
+
 function getAllUser()
 {
     global $users_txt;
@@ -109,4 +155,24 @@ function addUser($name, $username, $email, $role, $address, $phone, $gender, $da
         return false;
     }
     return true;
+}
+
+function getUserById($id)
+{
+    global $users_txt;
+    $userId = $id;
+    if ($userId) {
+        $usersFile = fopen($users_txt, 'r');
+        while (!feof($usersFile)) {
+            $data = fgets($usersFile);
+            if ($data != "") {
+                $user = explode('|', $data);
+                if (trim($user[0]) == $userId) {
+                    return array('id' => $user[0], 'username' => $user[1], 'email' => $user[2], 'name' => $user[3], 'role' => $user[4], 'address' => $user[5], 'gender' => $user[6], 'dateOfBirth' => $user[7], 'phone' => $user[8]);
+                }
+            }
+        }
+    }
+    setcookie('token', null, -1, '/');
+    return null;
 }
--- views/dashboard/roles/admin/manageUsers/userList.php
+++ views/dashboard/roles/admin/manageUsers/userList.php
@@ -26,24 +26,27 @@
         <td><input type="text" name="username"></td>
         <td><input type="submit" value="Add New"></td>
     </tr> -->
-    
+
     <?php
 
-for($i=0;$i<count($allUsers);++$i){
+    for ($i = 0; $i < count($allUsers); ++$i) {
     ?>
         <tr>
-            <td><?=$allUsers[$i]['id']?></td>
-            <td><?=$allUsers[$i]['username']?></td>
-            <td><?=$allUsers[$i]['email']?></td>
-            <td><?=$allUsers[$i]['name']?></td>
-            <td><?=$allUsers[$i]['role']?></td>
-            <td><?=$allUsers[$i]['address']?></td>
-            <td><?=$allUsers[$i]['gender']?></td>
-            <td><?=$allUsers[$i]['dateOfBirth']?></td>
-            <td><?=$allUsers[$i]['phone']?></td>
-            <td>Actions</td>
+            <td><?= $allUsers[$i]['id'] ?></td>
+            <td><?= $allUsers[$i]['username'] ?></td>
+            <td><?= $allUsers[$i]['email'] ?></td>
+            <td><?= $allUsers[$i]['name'] ?></td>
+            <td><?= $allUsers[$i]['role'] ?></td>
+            <td><?= $allUsers[$i]['address'] ?></td>
+            <td><?= $allUsers[$i]['gender'] ?></td>
+            <td><?= $allUsers[$i]['dateOfBirth'] ?></td>
+            <td><?= $allUsers[$i]['phone'] ?></td>
+            <td>
+                <a href="<?= "roles/admin/manageUsers/editUser.php?id=" . $allUsers[$i]['id'] ?>">Edit</a>
+                <a href="<?= "deleteUser.php?id=" . $allUsers[$i]['id'] ?>">Delete</a>
+            </td>
         </tr>
     <?php
-}
-?>
+    }
+    ?>
 </table>
