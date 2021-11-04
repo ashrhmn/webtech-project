e38a993d3b314abe7d24518ffafe7cd632232f8a
Ashik Rahman <ashrhmn99@gmail.com>
2021-11-05 03:46:10 +0600

    Add database.php file

---
+++ repository/database.php
@@ -0,0 +1,27 @@
+<?php
+function getConnection(){
+    $db_host = 'db';
+    // $db_port = 3307;
+    $db_name = 'dcms';
+    $db_user = 'dcms';
+    $db_password = 'ash';
+    $connection = new mysqli($db_host,$db_user,$db_password,$db_name);
+    if($connection->connect_error){
+        die('Connection Failed'.$connection->connect_error);
+    }
+    return $connection;
+}
+
+function mutate($sql){
+    $con = getConnection();
+    if($con->query($sql)===TRUE){
+        return true;
+    }
+    echo '<script>alert("'.$con->error.'")</script>';
+    return false;
+}
+
+function query($sql){
+    $con = getConnection();
+    return $con->query($sql);
+}
