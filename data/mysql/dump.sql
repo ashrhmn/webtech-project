c88c7960dbc9de2a3eabe571d746a82cb34de935
Ashik Rahman <ashrhmn99@gmail.com>
2021-11-10 14:31:27 +0600

    Add new dump.sql

--- data/mysql/dump.sql
+++ data/mysql/dump.sql
@@ -1,13 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 25, 2021 at 06:34 AM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
-
-set global max_allowed_packet=268435456;
+-- Host: db
+-- Generation Time: Nov 10, 2021 at 08:30 AM
+-- Server version: 8.0.26
+-- PHP Version: 7.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,18 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `equipments`
+--
+
+CREATE TABLE `equipments` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `count` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `session`
 --
 
--- CREATE Database DCMS;
-
--- use DCMS;
-
 CREATE TABLE `session` (
-  `id` int(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `token` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` int NOT NULL,
+  `userId` int NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `agent` varchar(255) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `session`
+--
+
+INSERT INTO `session` (`id`, `userId`, `token`, `agent`, `time`) VALUES
+(3, 6, 'c4df780fba2fbc109b7325c42018045ccbe61a140e294ad9ffd44b5f6a26eb1cfd96f65d9d', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0', '2021-11-09 21:08:12');
 
 -- --------------------------------------------------------
 
@@ -46,29 +61,36 @@ CREATE TABLE `session` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` enum('Admin','Doctor','Patient','Employee') NOT NULL DEFAULT 'Patient',
-  `address` text DEFAULT NULL,
+  `address` text,
   `phone` varchar(20) DEFAULT NULL,
   `gender` enum('Male','Female','Other','') DEFAULT NULL,
-  `dateOfBirth` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `dateOfBirth` date NOT NULL,
+  `profilePicture` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `role`, `address`, `phone`, `gender`, `dateOfBirth`) VALUES
-(6, 'Ashik Rahman', 'ash', 'ash@ash.com', 'ash', 'Admin', 'Shahzadpur Sirajgonj', '+8801746553758', 'Male', '1999-01-25'),
-(7, 'nil', 'nil', 'nil@nil.com', 'nil', 'Patient', 'djksubyhvjdshvb', 'ADJSUNBLKIJSDABV KL', 'Male', '2000-01-01');
+INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `role`, `address`, `phone`, `gender`, `dateOfBirth`, `profilePicture`) VALUES
+(6, 'Ashik Rahman', 'ash', 'ash@ash.com', 'ash', 'Admin', 'Shahzadpur Sirajgonj', '+8801746553758', 'Male', '1999-01-25', '../../assets/user/6-user1.png'),
+(9, 'nil rahman', 'nila', 'nila@nil.com', 'nil', 'Doctor', 'sasca', 'ascascasc', 'Male', '1999-11-09', 'assets/default.png');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `equipments`
+--
+ALTER TABLE `equipments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `session`
@@ -81,6 +103,8 @@ ALTER TABLE `session`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`),
   ADD KEY `users_index` (`id`);
 
 --
@@ -88,16 +112,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `equipments`
+--
+ALTER TABLE `equipments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `session`
 --
 ALTER TABLE `session`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
