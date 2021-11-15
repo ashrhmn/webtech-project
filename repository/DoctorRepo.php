4d0a5e646519d6fbbb50fdec4f3cd7f0faec5dfd
Ashik Rahman <ashrhmn99@gmail.com>
2021-11-16 03:49:09 +0600

    Complete migration-to-database system

--- repository/DoctorRepo.php
+++ repository/DoctorRepo.php
@@ -1,52 +1,27 @@
 <?php
 
 require_once('files.php');
+require_once('database.php');
 
 $users_txt = getFsRootDir() . "users.txt";
 $avDoctors_txt = getFsRootDir() . "availableDoctors.txt";
 
 function getAvailableDoctorsId(){
-    global $avDoctors_txt;
-    $docs = array();
-    $docFile = fopen($avDoctors_txt,'r');
-    while(!feof($docFile)){
-        $docId = fgets($docFile);
-        if($docId!=""){
-            array_push($docs,trim($docId));
-        }
-    }
-    return $docs;
+	$ids = array();
+	$idsAssoc = queryToAssocArray("select id from availableDoctors");
+	foreach($idsAssoc as $id){
+		array_push($ids,$id['id']);
+	}
+	return $ids;
+	
 }
 
+
+
 function toogleAvailability($id){
-    global $avDoctors_txt;
-    $docs = array();
-    $isAvailable = false;
-    $docFile = fopen($avDoctors_txt,'r');
-    while(!feof($docFile)){
-        $docId = fgets($docFile);
-        if($docId!=""){
-            if(trim($docId)==$id){
-                $isAvailable = true;
-            }
-            else{
-                array_push($docs,trim($docId));
-            }
-        }
-    }
-    fclose($docFile);
-    if($isAvailable){
-        $docFile = fopen($avDoctors_txt,'w');
-        for($i=0;$i<count($docs);++$i){
-            fwrite($docFile,$docs[$i]."\n");
-        }
-        fclose($docFile);
-        return true;
-    }
-    else{
-        $docFile = fopen($avDoctors_txt,'a');
-        fwrite($docFile,$id."\n");
-        fclose($docFile);
-        return true;
-    }
+	if(!isPreparedStatementExecuted("delete from availableDoctors where id=?",'i',$id)){
+		execPreparedStatement("insert into availableDoctors value(?)",'i',$id);
+	}
 }
+
+
