4f0d25e09510e6e3ffc05f8bec96183acd00ec91
Ashik Rahman <ashrhmn99@gmail.com>
2021-11-16 03:53:10 +0600

    Update sql file

--- data/mysql/dump.sql
+++ data/mysql/dump.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Nov 10, 2021 at 08:30 AM
--- Server version: 8.0.26
+-- Generation Time: Nov 15, 2021 at 09:51 PM
+-- Server version: 8.0.27
 -- PHP Version: 7.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -24,6 +24,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `availableDoctors`
+--
+
+CREATE TABLE `availableDoctors` (
+  `id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `availableDoctors`
+--
+
+INSERT INTO `availableDoctors` (`id`) VALUES
+(9),
+(10),
+(11),
+(12),
+(13);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `equipments`
 --
 
@@ -32,6 +53,13 @@ CREATE TABLE `equipments` (
   `name` varchar(255) NOT NULL,
   `count` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `equipments`
+--
+
+INSERT INTO `equipments` (`id`, `name`, `count`) VALUES
+(1, 'eq1', 8);
 
 -- --------------------------------------------------------
 
@@ -52,7 +80,8 @@ CREATE TABLE `session` (
 --
 
 INSERT INTO `session` (`id`, `userId`, `token`, `agent`, `time`) VALUES
-(3, 6, 'c4df780fba2fbc109b7325c42018045ccbe61a140e294ad9ffd44b5f6a26eb1cfd96f65d9d', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0', '2021-11-09 21:08:12');
+(3, 6, 'c4df780fba2fbc109b7325c42018045ccbe61a140e294ad9ffd44b5f6a26eb1cfd96f65d9d', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:94.0) Gecko/20100101 Firefox/94.0', '2021-11-09 21:08:12'),
+(5, 12, '246291a86a83133494a67f1b8886232a905d4912e396bb1f6dcfc0e0dd625fe0a2f23598aa', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0', '2021-11-15 21:51:03');
 
 -- --------------------------------------------------------
 
@@ -71,7 +100,7 @@ CREATE TABLE `users` (
   `phone` varchar(20) DEFAULT NULL,
   `gender` enum('Male','Female','Other','') DEFAULT NULL,
   `dateOfBirth` date NOT NULL,
-  `profilePicture` varchar(255) NOT NULL
+  `profilePicture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'default.png'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -79,12 +108,22 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `username`, `email`, `password`, `role`, `address`, `phone`, `gender`, `dateOfBirth`, `profilePicture`) VALUES
-(6, 'Ashik Rahman', 'ash', 'ash@ash.com', 'ash', 'Admin', 'Shahzadpur Sirajgonj', '+8801746553758', 'Male', '1999-01-25', '../../assets/user/6-user1.png'),
-(9, 'nil rahman', 'nila', 'nila@nil.com', 'nil', 'Doctor', 'sasca', 'ascascasc', 'Male', '1999-11-09', 'assets/default.png');
+(6, 'Ashik Rahman', 'ash', 'ash@ash.com', 'ash', 'Admin', 'Shahzadpur Sirajgonj', '+8801746553758', 'Male', '1999-01-25', 'default.png'),
+(9, 'nil rahman', 'nila', 'nila@nil.com', 'nil', 'Doctor', 'sasca', 'ascascasc', 'Male', '1999-11-09', 'default.png'),
+(10, 'asha', 'asha', 'asha@aa.aa', 'asha', 'Doctor', 'Shahzadpur, Sirajgonj', '2342342344444', 'Male', '2021-11-02', 'default.png'),
+(11, 'Ashik R', 'aaa', 'aaa@a.a', 'aaa', 'Doctor', 'kdisnhkfjasdhnklja', 'aaaaaaaaaaa', 'Male', '2021-11-15', 'default.png'),
+(12, 'DOctor 1', 'doc1', 'doc1@d.d', 'doc1', 'Doctor', 'jsdhlsdjvhb', 'bljinbl;', 'Male', '2001-01-01', 'default.png'),
+(13, 'jhdsblvjkhbd', 'doc2', 'doc2@d.d', 'doc2', 'Doctor', 'janslkjnas', 'jnkladjsnlvckj', 'Male', '2001-01-01', 'default.png');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `availableDoctors`
+--
+ALTER TABLE `availableDoctors`
+  ADD UNIQUE KEY `nvm` (`id`);
 
 --
 -- Indexes for table `equipments`
@@ -115,19 +154,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `equipments`
 --
 ALTER TABLE `equipments`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `session`
 --
 ALTER TABLE `session`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
