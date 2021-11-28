f1b905d54cf1da2c4ff44a0a37e583a919089bcf
rahmansabrina <sabrina.shoshi14@gmail.com>
2021-11-29 02:56:40 +0600

    employee update

--- views/dashboard/roles/employee/dashboard.php
+++ views/dashboard/roles/employee/dashboard.php
@@ -3,19 +3,5 @@
 echo 'Employee Dashboard'; 
 
 ?>
-<html>
-    <body>
-        <h1>BILL</h1>
-        <form action="../controller/handleBill.php" method="post">
-            <label>Tests</label>
-            <input type="text" name="test">
-            <label>Number of Test</label>
-            <input type="number" name="numoftest">
-			<label>Test Price</label>
-            <input type="number" name="test_price">
-			<label>Total Price</label>
-			<input type="number" name="total_price" value="">
-			<input type="submit" name="submit" value="Enter">
-        </form>
-    </body>
-</html>
+<a href="../../../../../views/dashboard/roles/employee/createbill.php"> Bill</a>
+<a href="appointment.php">Patient Appointment</a>
