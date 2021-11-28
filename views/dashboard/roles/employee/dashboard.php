a77bfb8729aa7323776f6665166f6da431404b07
Ashik Rahman <ashrhmn99@gmail.com>
2021-11-29 04:06:34 +0600

    fix dashboard layout

--- views/dashboard/roles/employee/dashboard.php
+++ views/dashboard/roles/employee/dashboard.php
@@ -1,7 +1,28 @@
 <?php
 
-echo 'Employee Dashboard'; 
+echo 'Employee Dashboard';
+
+
+require('secureRoute.php');
+
+// include('manageUsers/index.php');
 
 ?>
-<a href="../../../../../views/dashboard/roles/employee/createbill.php"> Bill</a>
-<a href="appointment.php">Patient Appointment</a>
+<br>
+<form action="#" method="POST">
+    <a href="?tab=appointment">Appointment</a>
+    <a href="?tab=createbill">Create Bill</a>
+</form>
+
+<hr>
+
+<?php
+
+if (isset($_GET['tab'])) {
+    include($_GET['tab'].'/index.php');
+    return;
+}
+include('appointment/index.php');
+?>
+<!-- <a href="../../../../../views/dashboard/roles/employee/createbill.php"> Bill</a>
+<a href="appointment.php">Patient Appointment</a> -->
