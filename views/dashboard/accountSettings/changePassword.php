75c834d1b983f43e68e337939e0f040a8a8f0568
Ashik Rahman <ashrhmn99@gmail.com>
2021-12-07 12:29:51 +0600

    Add vlaidation in password changing

--- views/dashboard/accountSettings/changePassword.php
+++ views/dashboard/accountSettings/changePassword.php
@@ -16,16 +16,50 @@ echo 'Change Password';
 			<td>New Password : </td>
 			<td>
 				<input type="password" name="newPassword">
+				<text id="error"></text>
 			</td>
 		</tr>
 		<tr>
 			<td>Confirm New Password : </td>
 			<td>
 				<input type="password" name="newPassword2">
+				<text id="error2"></text>
 			</td>
 		</tr>
 		<tr>
-			<td align="center" colspan="2"><input type="submit" name="savePasswordChanges" value="Save Changes"></td>
+			<td align="center" colspan="2"><input type="submit" name="savePasswordChanges" value="Save Changes" disabled></td>
 		</tr>
 	</table>
 </form>
+
+<script>
+	const validate = () => {
+		let newPassword = $("input[name='newPassword']").val();
+		let newPassword2 = $("input[name='newPassword2']").val();
+
+
+		if (newPassword.length < 8) {
+			$("#error").text("Password must be at least 8 characters");
+			return true;
+		}
+
+
+		if (newPassword != newPassword2) {
+			$("#error").text("Passwords do not match");
+			$("#error2").text("Passwords do not match");
+			return true;
+		}
+
+		$("#error").text("");
+		$("#error2").text("");
+
+		return false;
+	}
+
+	const setSubmissionState = () => {
+		$("input[name='savePasswordChanges']").attr('disabled', validate());
+	}
+
+	$('input[name="newPassword"]').on('keyup', setSubmissionState)
+	$('input[name="newPassword2"]').on('keyup', setSubmissionState)
+</script>
