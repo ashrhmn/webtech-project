a556b33dd70482c804511bce90e778b9db105546
Shovon <shovon.zzz5@gmail.com>
2021-12-13 13:47:32 +0600

    css added to homepage

--- views/home.php
+++ views/home.php
@@ -1,4 +1,20 @@
-<?php 
-    include("nav.php");
-    echo 'Home page';
+<?php
+include("nav.php");
+// echo 'Home page';
 ?>
+
+<br>
+<h2>
+    <center>Welcome to Homepage!</center>
+</h2>
+
+<style>
+    html {
+        background-color: #56baed;
+    }
+
+    body {
+        font-family: "Poppins", sans-serif;
+        height: 100vh;
+    }
+</style>
